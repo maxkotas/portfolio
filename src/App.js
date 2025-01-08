7ef1fcaac5dd10183599890d8faf90979a58b45d
maxkotas <38775745+maxkotas@users.noreply.github.com>
2025-01-08 17:49:45 -0600

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
 import {
-  BrowserRouter as Router,
   Route,
   Routes,
   Navigate
@@ -16,6 +15,7 @@ import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { HashRouter as Router } from "react-router-dom";
 
 function App() {
   const [load, upadateLoad] = useState(true);
